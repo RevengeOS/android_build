9377b403cad5db82186b1e2b0d5ed0f62bb0aa52
KreAch3R <kreach3r@users.noreply.github.com>
2015-05-01 21:24:55 +0300

    Remove generic builds. We never build them anyway. Change-Id: I6b35a3485195cf6a18ea0bb8bf22b70cb0e944c2

--- envsetup.sh
+++ envsetup.sh
@@ -1801,8 +1801,8 @@ else
 fi
 unset _xarray
 
-# Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
+# Execute the contents of any vendorsetup.sh files we can find (exluding generic vendorsetups).
+for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' ! -path '*/generic/*' 2> /dev/null | sort` \
          `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
          `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
 do
