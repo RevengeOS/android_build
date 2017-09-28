5721defc98aad90459f5ce05f4ec3880eae90e91
Christopher N. Hesse <raymanfx@gmail.com>
2017-09-28 21:13:38 +0200

    core: config: Use host bison if requested
    Change-Id: I87a232838cbc49ab758ed7bf27d97c59d9928e69

--- core/config.mk
+++ core/config.mk
@@ -630,7 +630,11 @@ LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
 # To run bison from elsewhere you need to set up enviromental variable
 # BISON_PKGDATADIR.
 BISON_PKGDATADIR := $(PWD)/prebuilts/build-tools/common/bison
+ifeq ($(USE_HOST_BISON),yes)
+BISON := bison
+else
 BISON := prebuilts/build-tools/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/bin/bison
+endif
 YACC := $(BISON) -d
 BISON_DATA := $(wildcard $(BISON_PKGDATADIR)/* $(BISON_PKGDATADIR)/*/*)
 
