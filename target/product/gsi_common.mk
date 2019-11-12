81cc362ac45c0cedbc7aaa1ef01c0ab0097f9839
Lucchetto <lucchetto.tie@live.com>
2019-11-12 20:44:03 +0100

    product: don't include AOSP ringtone props on custom builds
    Change-Id: I0d468f7bb66b932eb35f4114ae724413fef44aca
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- target/product/gsi_common.mk
+++ target/product/gsi_common.mk
@@ -28,10 +28,12 @@ $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.apex.updatable=false
 
+ifeq ($(REVENGEOS_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg \
+endif
 
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \
